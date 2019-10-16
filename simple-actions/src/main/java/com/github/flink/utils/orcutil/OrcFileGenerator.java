ca5393833369a3077a7b3b3d4d022ea35378dc6d
zlzhang0122 <zlzhang0122@gmail.com>
2019-10-16 18:39:24 +0800

    orc

---
+++ simple-actions/src/main/java/com/github/flink/utils/orcutil/OrcFileGenerator.java
@@ -0,0 +1,11 @@
+package com.github.flink.utils.orcutil;
+
+/**
+ * @Author: zlzhang0122
+ * @Date: 2019/10/16 5:45 PM
+ */
+public class OrcFileGenerator {
+    public static void main(String[] args){
+        Writer
+    }
+}
