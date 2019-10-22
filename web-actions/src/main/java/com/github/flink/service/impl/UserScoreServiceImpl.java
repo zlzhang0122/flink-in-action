6288ef87fe250a080eae1b6dd21644bf3c666203
zlzhang0122 <zlzhang0122@gmail.com>
2019-10-22 22:32:09 +0800

    data

--- web-actions/src/main/java/com/github/flink/service/impl/UserScoreServiceImpl.java
+++ web-actions/src/main/java/com/github/flink/service/impl/UserScoreServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,27 +28,74 @@ public class UserScoreServiceImpl implements UserScoreService {
 
     @Override
     public UserScoreEntity calUserScore(String userId) throws IOException {
+        Double[] colors = calColor(userId);
+        Double[] countrys = calcCountry(userId);
+        Double[] styles = calcStyle(userId);
 
+        UserScoreEntity userScoreEntity = new UserScoreEntity();
+        userScoreEntity.setUserId(userId);
+        userScoreEntity.setColor(colors);
+        userScoreEntity.setCountry(countrys);
+        userScoreEntity.setStyle(styles);
 
-        return null;
+        return userScoreEntity;
     }
 
     @Override
     public List<ProductScoreEntity> getProductScore(UserScoreEntity userScoreEntity) {
-        return null;
+        List<ProductScoreEntity> res = new ArrayList<>();
+        List<ProductEntity> productEntityList = getTopProductFrom(redisClient.getTopList(10));
+        int i = 0;
+        for(ProductEntity entity : productEntityList){
+            if(null != entity){
+                ProductScoreEntity scoreEntity = new ProductScoreEntity();
+                double v = calcProduct(userScoreEntity, entity);
+                scoreEntity.setScore(v);
+                scoreEntity.setRank(i++);
+                scoreEntity.setProductEntity(entity);
+
+                res.add(scoreEntity);
+            }
+        }
+
+        return res;
     }
 
     @Override
     public List<ProductScoreEntity> getTopRankProduct(String userId) throws IOException {
-        return null;
+        UserScoreEntity userScoreEntity = calUserScore(userId);
+        return getProductScore(userScoreEntity);
     }
 
     @Override
     public List<ProductEntity> getTopProductFrom(List<String> products) {
-        return null;
+        List<ProductEntity> top = new ArrayList<>();
+        for(String id : products){
+            ProductEntity entity;
+            if(null != id){
+                entity = productService.selectById(id);
+                top.add(entity);
+            }else{
+                top.add(null);
+            }
+        }
+
+        return top;
     }
 
-    private Double[] calcCountry(String userId) throws Exception {
+    private Double[] calcStyle(String userId) throws IOException {
+        int style0 = getValue(userId, "0");
+        int style1 = getValue(userId, "1");
+        int style2 = getValue(userId, "2");
+        int style3 = getValue(userId, "3");
+        int style4 = getValue(userId, "4");
+        int style5 = getValue(userId, "5");
+        int style6 = getValue(userId, "6");
+
+        return getPercent(style0, style1, style2, style3, style4, style5, style6);
+    }
+
+    private Double[] calcCountry(String userId) throws IOException {
         int china = getValue(userId, "china");
         int japan = getValue(userId, "japan");
         int korea = getValue(userId, "korea");
@@ -55,7 +103,7 @@ public class UserScoreServiceImpl implements UserScoreService {
         return getPercent(china, japan, korea);
     }
 
-    private Double[] calColor(String userId) throws Exception {
+    private Double[] calColor(String userId) throws IOException {
         int red = getValue(userId, "red");
         int green = getValue(userId, "green");
         int black = getValue(userId, "black");
@@ -93,5 +141,78 @@ public class UserScoreServiceImpl implements UserScoreService {
         }
 
         return res;
+    }
+
+    private double calcProduct(UserScoreEntity userScoreEntity, ProductEntity entity){
+        String color = entity.getColor();
+        String country = entity.getCountry();
+        String style = entity.getStyle();
+
+        double val = 0.0;
+
+        switch (color){
+            case "red":
+                val += userScoreEntity.getColor()[0] == null ? 0 : userScoreEntity.getColor()[0];
+                break;
+            case "green":
+                val += userScoreEntity.getColor()[1] == null ? 0 : userScoreEntity.getColor()[1];
+                break;
+            case "black":
+                val += userScoreEntity.getColor()[2] == null ? 0 : userScoreEntity.getColor()[2];
+                break;
+            case "brown":
+                val += userScoreEntity.getColor()[3] == null ? 0 : userScoreEntity.getColor()[3];
+                break;
+            case "grey":
+                val += userScoreEntity.getColor()[4] == null ? 0 : userScoreEntity.getColor()[4];
+                break;
+            default:
+                val += 0;
+                break;
+        }
+
+        switch (country){
+            case "china":
+                val += userScoreEntity.getCountry()[0] == null ? 0 : userScoreEntity.getCountry()[0];
+                break;
+            case "green":
+                val += userScoreEntity.getCountry()[1] == null ? 0 : userScoreEntity.getCountry()[1];
+                break;
+            case "black":
+                val += userScoreEntity.getCountry()[2] == null ? 0 : userScoreEntity.getCountry()[2];
+                break;
+            default:
+                val += 0;
+                break;
+        }
+
+        switch (style){
+            case "0":
+                val += userScoreEntity.getStyle()[0] == null ? 0 : userScoreEntity.getStyle()[0];
+                break;
+            case "1":
+                val += userScoreEntity.getStyle()[1] == null ? 0 : userScoreEntity.getStyle()[1];
+                break;
+            case "2":
+                val += userScoreEntity.getStyle()[2] == null ? 0 : userScoreEntity.getStyle()[2];
+                break;
+            case "3":
+                val += userScoreEntity.getStyle()[3] == null ? 0 : userScoreEntity.getStyle()[3];
+                break;
+            case "4":
+                val += userScoreEntity.getStyle()[4] == null ? 0 : userScoreEntity.getStyle()[4];
+                break;
+            case "5":
+                val += userScoreEntity.getStyle()[5] == null ? 0 : userScoreEntity.getStyle()[5];
+                break;
+            case "6":
+                val += userScoreEntity.getStyle()[6] == null ? 0 : userScoreEntity.getStyle()[6];
+                break;
+            default:
+                val += 0;
+                break;
+        }
+
+        return val;
     }
 }
