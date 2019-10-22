7561db2913add0a4050b2c85cb7787146f93b6d5
zlzhang0122 <zlzhang0122@gmail.com>
2019-10-22 21:10:12 +0800

    data

--- web-actions/src/main/java/com/github/flink/controller/RecommandController.java
+++ web-actions/src/main/java/com/github/flink/controller/RecommandController.java
@@ -1,16 +1,23 @@
 package com.github.flink.controller;
 
+import com.github.flink.dto.ProductDto;
 import com.github.flink.service.KafkaService;
 import com.github.flink.service.RecommandService;
 import com.github.flink.utils.Result;
 import com.github.flink.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
+ * 推荐页
+ *
  * @Author: zlzhang0122
  * @Date: 2019/10/22 7:43 PM
  */
@@ -21,6 +28,21 @@ public class RecommandController {
 
     @Autowired
     private KafkaService kafkaService;
+
+    @GetMapping("/recommand")
+    public String recommandByUserId(@RequestParam("userId") String userId,
+                                    Model model) throws IOException{
+        List<ProductDto> hotList = recommandService.recommandByHotList();
+        List<ProductDto> itemCfCoeffList = recommandService.recommandByItemCfCoeff();
+        List<ProductDto> productCoeffList = recommandService.recommandByProductCoeff();
+
+        model.addAttribute("userId", userId);
+        model.addAttribute("hotList", hotList);
+        model.addAttribute("itemCfCoeffList", itemCfCoeffList);
+        model.addAttribute("productCoeffList", productCoeffList);
+
+        return "user";
+    }
 
     @GetMapping("/log")
     @ResponseBody
