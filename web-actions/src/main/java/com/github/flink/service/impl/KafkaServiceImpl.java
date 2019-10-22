f6f12dbb567c26bba0e334c690c19bbe49b814ec
zlzhang0122 <zlzhang0122@gmail.com>
2019-10-22 21:16:58 +0800

    data

---
+++ web-actions/src/main/java/com/github/flink/service/impl/KafkaServiceImpl.java
@@ -0,0 +1,19 @@
+package com.github.flink.service.impl;
+
+import com.github.flink.service.KafkaService;
+
+/**
+ * @Author: zlzhang0122
+ * @Date: 2019/10/22 9:16 PM
+ */
+public class KafkaServiceImpl implements KafkaService {
+    @Override
+    public void send(String key, String value) {
+
+    }
+
+    @Override
+    public String makeLog(String userId, String productId, String action) {
+        return null;
+    }
+}
