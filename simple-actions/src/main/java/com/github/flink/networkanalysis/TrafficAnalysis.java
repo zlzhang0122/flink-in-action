05e7d314fa55e7fd8fb63696c973342238bed940
zlzhang0122 <zlzhang0122@gmail.com>
2019-10-28 19:14:17 +0800

    trafficanalysis

---
+++ simple-actions/src/main/java/com/github/flink/networkanalysis/TrafficAnalysis.java
@@ -0,0 +1,23 @@
+package com.github.flink.networkanalysis;
+
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 网络流量分析,每5秒统计一次10分钟内访问量最高的10个url
+ *
+ * @Author: zlzhang0122
+ * @Date: 2019/10/28 7:10 PM
+ */
+public class TrafficAnalysis {
+    private static final Logger logger = LoggerFactory.getLogger(TrafficAnalysis.class);
+
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+
+    }
+}
