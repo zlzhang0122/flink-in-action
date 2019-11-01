4f4672b027a53b0a247c8441ff79024b4a9f1860
zlzhang0122 <zlzhang0122@gmail.com>
2019-11-01 20:00:12 +0800

    streamA

--- simple-actions/src/main/java/com/github/flink/streamjoin/StreamJoinAction.java
+++ simple-actions/src/main/java/com/github/flink/streamjoin/StreamJoinAction.java
@@ -1,10 +1,17 @@
 package com.github.flink.streamjoin;
 
+import com.github.flink.streamjoin.model.StockTransaction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,6 +21,8 @@ import java.util.List;
  * @Date: 2019/10/31 6:56 PM
  */
 public class StreamJoinAction {
+    private static final Logger logger = LoggerFactory.getLogger(StreamJoinAction.class);
+
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
@@ -27,7 +36,27 @@ public class StreamJoinAction {
         listA.add("2019-08-08 13:00:05.990,000001,10.0");
         listA.add("2019-08-08 13:00:14.000,000001,10.1");
         listA.add("2019-08-08 13:00:20.000,000001,10.2");
-        DataStream<String> dataStreamA = env.fromCollection(listA);
+        DataStream<StockTransaction> dataStreamA = env.fromCollection(listA).map(new MapFunction<String, StockTransaction>() {
+            @Override
+            public StockTransaction map(String value) throws Exception {
+                String[] s = value.split(",");
+
+                return new StockTransaction(s[0], s[1], Double.parseDouble(s[2]));
+            }
+        }).assignTimestampsAndWatermarks(new AscendingTimestampExtractor<StockTransaction>() {
+            @Override
+            public long extractAscendingTimestamp(StockTransaction element) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+                try{
+                    Date date = simpleDateFormat.parse(element.getTxTime());
+                    return date.getTime();
+                }catch (Exception e){
+                    logger.error("parse error", e);
+                    return Integer.MAX_VALUE;
+                }
+            }
+        });
 
         List<String> listB = new ArrayList<>();
         listA.add("2019-08-08 13:00:01.000,000001,10.2");
