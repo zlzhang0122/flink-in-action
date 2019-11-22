cc6c5fc9b259e28f2e2875c3e322111a8d700616
zlzhang0122 <zlzhang0122@gmail.com>
2019-11-22 13:55:00 +0800

    trigger

--- simple-actions/src/main/java/com/github/flink/continuouseventtime/ContinuousEventTimeTriggerDemo.java
+++ simple-actions/src/main/java/com/github/flink/continuouseventtime/ContinuousEventTimeTriggerDemo.java
@@ -1,8 +1,19 @@
 package com.github.flink.continuouseventtime;
 
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * @Author: zlzhang0122
  * @Date: 2019/11/22 1:30 PM
  */
 public class ContinuousEventTimeTriggerDemo {
+    public static void main(String[] args){
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setAutoWatermarkInterval(5000L);
+        env.setMaxParallelism(1);
+
+
+    }
 }
