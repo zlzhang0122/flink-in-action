02edd984513bf6ceff134f2eaf6a84127d066c6b
zlzhang0122 <zlzhang0122@gmail.com>
2020-09-16 20:12:03 +0800

    [flink-in-action]uv

---
+++ simple-actions/src/main/java/com/github/flink/realtimeuv/RealtimeUV.java
@@ -0,0 +1,37 @@
+package com.github.flink.realtimeuv;
+
+import akka.remote.serialization.ProtobufSerializer;
+import com.github.flink.utils.PropertiesUtil;
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+
+import java.util.Properties;
+
+/**
+ * 实时UV计算
+ *
+ * @Author: zlzhang0122
+ * @Date: 2020/9/16 4:18 下午
+ */
+public class RealtimeUV {
+    public static void main(String[] args){
+
+        Properties sourceTopic = PropertiesUtil.getKafkaProperties("uv.group");
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+//        env.getConfig().registerTypeWithKryoSerializer(MobilePage.class,
+//                ProtobufSerializer.class);
+
+
+
+//        String topic = config.get("source.kafka.topic");
+//        String groupId = config.get("source.group.id");
+//        String sourceBootStrapServers = config.get("source.bootstrap.servers");
+//        String hbaseTable = config.get("hbase.table.name");
+//        String hbaseZkQuorum = config.get("hbase.zk.quorum");
+//        String hbaseZkParent = config.get("hbase.zk.parent");
+//        int checkPointPeriod = Integer.parseInt(config.get("checkpoint.period"));
+//        int checkPointTimeout = Integer.parseInt(config.get("checkpoint.timeout"));
+
+
+    }
+}
---
+++ simple-actions/src/main/java/com/github/flink/utils/ProtobufSerializer.java
@@ -0,0 +1,9 @@
+package com.github.flink.utils;
+
+/**
+ * @Author: zlzhang0122
+ * @Date: 2020/9/16 4:50 下午
+ */
+public class ProtobufSerializer {
+
+}
